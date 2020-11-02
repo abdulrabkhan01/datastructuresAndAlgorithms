76c548dbc8fb5857fb2ba69857e2538158f75f4c
abdulrabkhan01 <abdul.lambent@gmail.com>
2020-11-03 00:42:15 +0530

    Added more unit tests for testing stack class

--- src/test/java/com/ark/ds/stacks/StacksSpec.java
+++ src/test/java/com/ark/ds/stacks/StacksSpec.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StacksSpec {
 
     @Test
-    @DisplayName("given Empty Stack Pop Should Throw ElementNotFoundException Exception")
+    @DisplayName("Given Empty Stack Pop Should Throw ElementNotFoundException Exception")
     public void emptyStackSpec() {
         //Given - An Empty Stack
         IStack<String> stack = new Stack<>(1);
@@ -19,7 +19,7 @@ public class StacksSpec {
     }
 
     @Test
-    @DisplayName("given Empty Stack When one element added its size should become one")
+    @DisplayName("Given Empty Stack When one element added its size should become one")
     public void emptyStackFirstElementAddSpec() {
         //Given - An Empty Stack
         IStack<String> stack = new Stack<>(1);
@@ -30,7 +30,49 @@ public class StacksSpec {
     }
 
     @Test
-    @DisplayName("given  Stack with one elemnt When pop operation is called  its size should become zero")
+    @DisplayName("Given  Stack with no element When isEmpty method is called  its should return boolean true")
+    public void isEmptySpecWhenStackIsEmpty() {
+        //Given - An Empty Stack
+        IStack<String> stack = new Stack<>(1);
+        //When no elements present
+        //Then isEmpty Method should return true
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Given  Stack with no element once element added and isEmpty() method called,  its should return boolean false")
+    public void isEmptySpecWhenStackIsNotEmpty() {
+        //Given - An Empty Stack
+        IStack<String> stack = new Stack<>(1);
+        //When an element added
+        stack.push("A");
+        //Then isEmpty Method should return false
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Given  Stack with no element When isEmpty method is called  its should return size as 0")
+    public void sizeSpecWhenStackIsEmpty() {
+        //Given - An Empty Stack
+        IStack<String> stack = new Stack<>(1);
+        //When no elements present
+        //Then isEmpty Method should return true
+        assertTrue(stack.size()==0);
+    }
+
+    @Test
+    @DisplayName("Given  Stack with no element once element added and isEmpty() method called,  its should return size as 1")
+    public void sizeSpecWhenStackIsNotEmpty() {
+        //Given - An Empty Stack
+        IStack<String> stack = new Stack<>(1);
+        //When an element added
+        stack.push("A");
+        //Then isEmpty Method should return false
+        assertTrue(stack.size()==1);
+    }
+
+    @Test
+    @DisplayName("Given  Stack with one element When pop operation is called  its size should become zero")
     public void stackWithOneElementPushAndPopSpec() {
         //Given - An Empty Stack
         IStack<String> stack = new Stack<>(1);
@@ -41,5 +83,6 @@ public class StacksSpec {
         stack.pop();
         assertTrue(stack.isEmpty());
     }
+
 
 }
