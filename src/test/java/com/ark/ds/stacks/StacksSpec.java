e6d79479e178a7ac403ee7165d3a0dabb6a5bdcb
abdulrabkhan01 <abdul.lambent@gmail.com>
2020-11-03 00:05:42 +0530

    Refactored test to use Junit 5.7 version

--- src/test/java/com/ark/ds/stacks/StacksSpec.java
+++ src/test/java/com/ark/ds/stacks/StacksSpec.java
@@ -1,30 +1,32 @@
 package com.ark.ds.stacks;
 
-import org.junit.Assert;
-import org.junit.Test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StacksSpec {
 
 
-    @Test(expected = ElementNotFoundException.class)
-    public void givenEmptyStackPopShouldThrowException() {
+    @Test
+    @DisplayName("given Empty Stack Pop Should Throw ElementNotFoundException Exception")
+    public void emptyStackSpec() {
         //Given - An Empty Stack
         IStack<String> stack = new Stack<>();
         //When user tries to do the POP operation
-        stack.pop();
+        assertThrows(ElementNotFoundException.class,()-> {stack.pop();});
         //Exception should be encountered
     }
 
-    public void givenOneElementAddedToEmptyStackSizeShouldBeOne() {
+    @Test
+    @DisplayName("given Empty Stack When one element added its size should become one")
+    public void emptyStackFirstElementAddSpec() {
         //Given - An Empty Stack
         IStack<String> stack = new Stack<>();
         //When an element added
         stack.push("A");
         //Then its size should be 1
-        Assert.assertEquals(1,stack.size());
-
-
+        assertEquals(1,stack.size());
     }
 }
