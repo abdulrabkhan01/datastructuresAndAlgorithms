45f67342a18f43bf3dae2d8730d565836b919a5f
abdulrabkhan01 <abdul.lambent@gmail.com>
2020-11-07 23:37:33 +0530

    Added more tests

--- src/test/java/com/ark/ds/graphs/shortestpath/dijktras/DijktrasShortestPathSpec.java
+++ src/test/java/com/ark/ds/graphs/shortestpath/dijktras/DijktrasShortestPathSpec.java
@@ -18,12 +18,7 @@ public class DijktrasShortestPathSpec {
     @Test
     public void dijktrasShortestPathSpec1() {
         IGraph graph = new Graph();
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
-        graph.addEdge("A", "B", 1);
-        graph.addEdge("B", "C", 2);
-        graph.addEdge("A", "C", 5);
+        initializeGraph(graph);
         IShortestPath dijktrasShortestPath = new DijktrasShortestPath();
         List<Vertex> shortestPath = dijktrasShortestPath.findShortestPath(graph, "A", "C");
         StringBuilder path = new StringBuilder(shortestPath.size());
@@ -31,6 +26,41 @@ public class DijktrasShortestPathSpec {
             path.append(v.getLabel());
         }
         assertEquals("ABC",path.toString());
+    }
+
+    @Test
+    public void dijktrasShortestPathSpec2() {
+        IGraph graph = new Graph();
+        initializeGraph(graph);
+        IShortestPath dijktrasShortestPath = new DijktrasShortestPath();
+        List<Vertex> shortestPath = dijktrasShortestPath.findShortestPath(graph, "A", "B");
+        StringBuilder path = new StringBuilder(shortestPath.size());
+        for(Vertex v : shortestPath) {
+            path.append(v.getLabel());
+        }
+        assertEquals("AB",path.toString());
+    }
+
+    @Test
+    public void dijktrasShortestPathSpec3() {
+        IGraph graph = new Graph();
+        initializeGraph(graph);
+        IShortestPath dijktrasShortestPath = new DijktrasShortestPath();
+        List<Vertex> shortestPath = dijktrasShortestPath.findShortestPath(graph, "B", "C");
+        StringBuilder path = new StringBuilder(shortestPath.size());
+        for(Vertex v : shortestPath) {
+            path.append(v.getLabel());
+        }
+        assertEquals("BC",path.toString());
+    }
+
+    private void initializeGraph(IGraph graph) {
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addEdge("A", "B", 1);
+        graph.addEdge("B", "C", 2);
+        graph.addEdge("A", "C", 5);
     }
 
 
