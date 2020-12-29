ade4c2421349a34255ee3a6cc7cebc3f78d8c82a
abdulrabkhan01 <abdul.lambent@gmail.com>
2020-12-30 02:02:31 +0530

    Fixed the code

--- src/main/java/com/ark/algo/dynamicprogramming/LongestCommonSubSequence.java
+++ src/main/java/com/ark/algo/dynamicprogramming/LongestCommonSubSequence.java
@@ -13,9 +13,9 @@ public class LongestCommonSubSequence {
         int l2 = s2.length();
         //Table for memorization
         int[][] table = new int[l1 + 1][l2 + 1];
-        initializeTable(table);
         //populate the table & calculate lcs length
         int lcsLength = lcs(s1, s2, l1, l2, table);
+        System.out.println("LCS Length " + lcsLength);
         //create array to store lcs using the table
         String lcsValue = getLCSString(s1, s2, l1, l2, table, lcsLength);
         return lcsValue;
@@ -25,7 +25,7 @@ public class LongestCommonSubSequence {
         char[] lcs = new char[lcsLength];
         int i = l1, j = l2, index = lcsLength - 1;
         while (i > 0 && j > 0) {
-            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            if (s1.charAt(i-1) == s2.charAt(j-1)) {
                 //Matching char
                 lcs[index] = s1.charAt(i - 1);
                 //Move diagonally back in table
@@ -43,31 +43,27 @@ public class LongestCommonSubSequence {
         return String.valueOf(lcs);
     }
 
-    private static void initializeTable(int[][] table) {
-        //Fill all rows with -1
-        for (int[] row : table) {
-            Arrays.fill(row, -1);
-        }
-    }
-
     private static int lcs(String s1, String s2, int l1, int l2, int[][] table) {
-        if (l1 == 0 || l2 == 0) {
-            return 0;
+        for (int row = 0; row <= l1; row++) {
+            for (int column = 0; column <= l2; column++) {
+                if (row == 0 || column == 0) {
+                    table[row][column] = 0;
+                } else if (s1.charAt(row - 1) == s2.charAt(column - 1)) {
+                    //diagonal
+                    table[row][column] = table[row - 1][column - 1] + 1;
+                } else {
+                    //Max of previous row or previous column
+                    table[row][column] = Math.max(table[row - 1][column], table[row][column - 1]);
+                }
+            }
         }
-        if (table[l1 - 1][l2 - 1] != -1) {
-            return table[l1 - 1][l2 - 1];
-        }
-        if (s1.charAt(l1 - 1) == s2.charAt(l2 - 1)) {
-            table[l1 - 1][l2 - 1] = 1 + lcs(s1, s2, l1 - 1, l2 - 1, table);
-        } else {
-            table[l1 - 1][l2 - 1] = Math.max(lcs(s1, s2, l1 - 1, l2, table), lcs(s1, s2, l1, l2 - 1, table));
-        }
-        return table[l1 - 1][l2 - 1];
+        return table[l1][l2];
+
     }
 
     public static void main(String[] args) {
-        String s1 = "axybanzti";
-        String s2 = "aynbcnzto";
+        String s1 = "acc";
+        String s2 = "ac";
         String lcs = getLCS(s1, s2);
         System.out.println(lcs);
     }
